17552ab84915ac0893fdd85019f6666df5471dd6
athacker <andrea.thacker@outlook.net>
2016-06-30 14:00:13 -0600

    Save User Profile -- via MEAN StAcK!

--- api/services/profile.js
+++ api/services/profile.js
@@ -5,11 +5,8 @@ var jwt = require('jwt-simple');
 
 module.exports.get = function(req, res){
 console.log('Get User: ' + req.params.id );
-
-
     if(!req.headers.authorization){
-        console.log('No authorization in header...');
-        res.status(401).send({message:'You are not authorized to view jobs.'});
+        res.status(401).send({message:'You are not authorized to view this profile.'});
         return;
     }
     var token = req.headers.authorization.split(' ')[1];
@@ -18,14 +15,15 @@ console.log('Get User: ' + req.params.id );
         res.status(401).send({message: 'Authentication Failed.'});
     }
 
-
-    var profile=[];
-
-    res.json(profile);
+    var user={id:req.params.id, displayName:'John Doe' };
+    return res.json(user);
 };
 
 module.exports.post=function(req,res){
     console.log('Post User: ' + req.body.user.id );
+    req.body.user.first='YOUR';
+    req.body.user.last='SAVED';
+   // var user={id:req.body.user.id, displayName:'Target' };
+    return res.json(req.body.user);
 
-    res.json({});
 }
