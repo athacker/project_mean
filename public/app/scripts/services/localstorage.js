844a7e023a5ba7adfaa7e291785c7eb9211e2b3b
athacker <andrea.thacker@outlook.net>
2016-07-01 16:26:03 -0600

    Added Local Storage Service to hold user data. Used rootScope to manage event -- probably should put that into a service too -- but ok for right now.

---
+++ public/app/scripts/services/localstorage.js
@@ -0,0 +1,40 @@
+'use strict';
+
+angular.module('secureApp').service('localStorage', function ($rootScope, $window) {
+
+  var storage = $window.localStorage;
+
+  var setUser = function(user){
+    if(!user.displayName){
+      user.displayName = user.email;
+    }
+    storage.setItem('displayName', user.displayName);
+    storage.setItem('userId', user.id);
+    $rootScope.$broadcast('user_logged_event');
+ };
+
+  var getDisplayName = function(){
+      return storage.getItem('displayName');
+  }
+
+  var getUserId = function(){
+      return storage.getItem('userId');
+  }
+
+  var clearStorage=function(){
+    storage.removeItem('userId');
+    storage.removeItem('displayName');
+  }
+
+
+  return{
+    setUser:setUser,
+    getDisplayName:getDisplayName,
+    getUserId:getUserId,
+    clearStorage:clearStorage
+  }
+
+
+
+
+});
